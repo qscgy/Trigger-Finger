1c10923fdce5a54c33dd8d21dbac5682cbc074f3
Sam Ehrenstein <sam@davids-air.home>
2014-11-07 21:37:39 -0500

    Initial Commit

---
+++ Trigger Finger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trigger Finger
+//
+//  Created by Sam Ehrenstein on 11/7/14.
+//  Copyright (c) 2014 SmartPhone Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
